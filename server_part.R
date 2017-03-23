5dffe8c5aa477542106ccd334f3ab980406a62d1
longhow lam <longhowlam@gmail.com>
2017-03-23 10:30:23 +0100

    C1

---
+++ server_part.R
@@ -0,0 +1,60 @@
+NodesDF = readRDS("data/NodesDF.RDs")
+RelatedMovies = readRDS("data/RelatedMovies.RDs")
+
+server = function(input, output, session) {
+  
+  
+  ##### Introduction TAB #################################################################################
+  
+  output$intro <- renderUI({
+    list(
+     
+      h4("Videoland samenvattingen........  beschrijving....")
+      
+      
+      )
+    
+  })
+  
+  ##### Network tab ###########################################
+
+  output$MovieLinks <- renderVisNetwork({
+  
+    NodesDF = NodesDF %>% arrange(label)
+    
+    visNetwork(
+      nodes = NodesDF, 
+      edges = RelatedMovies,  
+      main = "network of movies close to each other"
+    ) %>% 
+      visLegend() %>%
+      visOptions(
+        highlightNearest = list(enabled = T, degree = 0, hover = FALSE),
+        nodesIdSelection = TRUE
+      ) %>%
+      visInteraction(
+        navigationButtons = TRUE
+      ) %>%
+      visIgraphLayout(
+        layout = "layout_with_drl", options=list(simmer.attraction=0),
+        physics=FALSE
+      ) %>%
+      visEdges(smooth = FALSE)
+    
+  })
+  
+  output$MovieTable <- renderUI({
+    movies  = input$MovieLinks_selected
+    if (is.null(movies)){
+      print("No movie selected")
+    }
+    else
+     eenfilm = NodesDF %>% 
+        filter(id == as.numeric(movies)) %>% 
+        select(label, description, runtime)
+      
+    print(eenfilm$description)
+    #print(stringr::str_replace_all(eenfilm$description, "\\.", "\n"))
+  })
+ 
+}
---
+++ ui_part.R
@@ -0,0 +1,30 @@
+ui <- dashboardPage(
+  dashboardHeader(title = "Videoalnd Movies Network", titleWidth = "330px"),
+  dashboardSidebar(width = 200,
+                   
+                   sidebarMenu(
+                     menuItem("Introduction", tabName = "Introduction", icon = icon("euro")),
+                     menuItem("Movies Network", tabName = "ArtistNetwork2", icon = icon("link"))
+                   ) 
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "Introduction",
+              titlePanel("Introduction"),
+              mainPanel(
+                htmlOutput("intro")
+              )
+      ),
+      
+      tabItem(
+        tabName = "ArtistNetwork2",
+        mainPanel( width = 12,
+          fluidRow(
+            column(8, visNetworkOutput("MovieLinks", height = "700px")),
+            column(4, htmlOutput("MovieTable"))
+          )
+        )
+      )
+    )
+  )
+)
